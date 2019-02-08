7af5075944636760b1e60421aaadf8ecd37d77e2
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-02-07 20:44:26 -0500

    added yargs module, added a command to handle the touch file

---
+++ src/commands/createMigrationFile.js
@@ -0,0 +1,37 @@
+// const readline = require('readline');
+
+const createFileName = require('../generators/fileName/createFileName');
+
+// const yargs = require('yargs');
+// yargs.option('chad');
+
+module.exports = {
+  command: 'abc',
+  desc: 'touch a file',
+  handler: argv => {
+    const fileName = createFileName();
+    console.log(`Saved! ${fileName}`);
+  }
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  //   prompt: `touch ${fileName}? (y/n): `
+  // });
+
+  // rl.prompt();
+  // rl.on('line', line => {
+  //   switch (line.trim()) {
+  //   case 'n':
+  //     return rl.close();
+  //   case 'y':
+  //     // fs.writeFile(fileName, null, err => err && Error.throw(err));
+  //     // eslint-disable-next-line no-console
+
+  //     return rl.close();
+  //   default:
+  //     break;
+  //   }
+  //   return rl.prompt();
+  // }).on('close', () => {
+  // });
+};
