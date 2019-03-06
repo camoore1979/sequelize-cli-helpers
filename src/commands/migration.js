3b5fa52f3afe1aad8d8beea8cac71a466789c910
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-03-06 18:13:15 -0500

    #9: stubbed out (non-working) migration command, WIP

--- src/commands/migration.js
+++ src/commands/migration.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const config = require('../config');
-const touchFile = require('../handlers/touchFile');
+const path = require('path');
+// const touchFile = require('../handlers/touchFile');
+
+const logger = require('../lib/logger');
+
+const { yesNo } = require('../prompts/');
+const getFileNameParts = require('../handlers/helpers/getFileNameParts');
+const generateFileName = require('../generators/generateFileName');
+const generateTableMigration = require('../generators/generateTableMigration');
+const writeFile = require('../lib/writeFile');
 
 // add ability to pass in various settings... such as path to created file
 const builder = yargs => {
@@ -21,7 +30,30 @@ module.exports = {
   desc: 'generates a migration file',
   builder,
   handler: async (/* argv */) => {
-    const fileName = await touchFile(config);
-    console.log('created file: ', fileName);
+    // const fileName = await touchFile(config);
+
+    const migrationContent = generateTableMigration('material_subgroup');
+
+    const {
+      sequelize: { 'migrations-path': migrationsPath },
+      settings
+    } = config;
+  
+    // const {  } = settings;
+    const fileNameParts = await getFileNameParts(config);
+    const fileName = generateFileName({
+      ...settings,
+      fileNameParts
+    });
+  
+    const confirm = await yesNo(`Create migration with name "${fileName}"?`);
+    if (confirm) {
+      const dir = path.join(migrationsPath, fileName);
+      writeFile(dir, migrationContent);
+      logger.log(`created "${fileName}!`);
+    } else {
+      logger.log('cancelling... no migration created.');
+    }
+  
   }
 };
--- src/handlers/touchFile.js
+++ src/handlers/touchFile.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const logger = require('../lib/logger');
 
-const handleFileNameGeneration = require('./helpers/handleFileNameGeneration');
+const { yesNo } = require('../prompts/');
+const getFileNameParts = require('./helpers/getFileNameParts');
+const generateFileName = require('../generators/generateFileName');
 const writeFile = require('../lib/writeFile');
 
 /**
@@ -13,10 +15,20 @@ const writeFile = require('../lib/writeFile');
  * @param {object} options
  */
 module.exports = async options => {
-  const { sequelize: { 'migrations-path': migrationsPath } } = options;
+  const {
+    sequelize: { 'migrations-path': migrationsPath },
+    settings
+  } = options;
 
-  const fileName = await handleFileNameGeneration(options);
-  if (fileName) {
+  // const {  } = settings;
+  const fileNameParts = await getFileNameParts(options);
+  const fileName = generateFileName({
+    ...settings,
+    fileNameParts
+  });
+
+  const confirm = await yesNo(`Create file with name "${fileName}"?`);
+  if (confirm) {
     const dir = path.join(migrationsPath, fileName);
     writeFile(dir);
     logger.log(`created "${fileName}!`);
