dfcdb03ebd3356c136769e8c6637004fef1b4a82
Chad Allen Moore <mr.chad.moore@gmail.com>
2019-03-09 16:24:32 -0500

    #9: fixed temp lint errors, WIP

--- src/generators/helpers/generateFromTemplate.js
+++ src/generators/helpers/generateFromTemplate.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const handlebars = require('handlebars');
 
 const getTemplate = templatePath => {
 
   // if pathToTemplate is NOT absolute, then it should be a template NAME
   // then look in a common dir for that e.g., ../templates
-  const { dir: parsedDir } = path.parse(module.filename);
-  const pathToTemplate = path.resolve(parsedDir, '../templates/migrations/create_table.hbs');
+  // const { dir: parsedDir } = path.parse(module.filename);
+  // const pathToTemplate = path.resolve(parsedDir, '../templates/migrations/create_table.hbs');
 
   return fs.readFileSync(templatePath, 'UTF-8');
 };
